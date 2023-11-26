7e2b25b18a59b2f9c58fad23f4e96cdf66ecddac
honoody <me@honoody.ru>
2023-11-27 01:34:26 +0300

    done hw5

--- src/samples/simple_compute/simple_compute.cpp
+++ src/samples/simple_compute/simple_compute.cpp
@@ -3,6 +3,8 @@
 #include <vk_pipeline.h>
 #include <vk_buffers.h>
 #include <vk_utils.h>
+#include <random>
+#include <chrono>
 
 SimpleCompute::SimpleCompute(uint32_t a_length) : m_length(a_length)
 {
@@ -12,6 +14,8 @@ SimpleCompute::SimpleCompute(uint32_t a_length) : m_length(a_length)
   m_enableValidation = true;
 #endif
 }
+
+
 
 void SimpleCompute::SetupValidationLayers()
 {
@@ -69,41 +73,47 @@ void SimpleCompute::CreateDevice(uint32_t a_deviceId)
   vkGetDeviceQueue(m_device, m_queueFamilyIDXs.transfer, 0, &m_transferQueue);
 }
 
+float generateRandomFloat(float min, float max) {
+    float randomNormalized = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+    return min + (randomNormalized * (max - min));;
+}
+
+
 
 void SimpleCompute::SetupSimplePipeline()
 {
   std::vector<std::pair<VkDescriptorType, uint32_t> > dtypes = {
       {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,             3}
   };
-
+  srand(time(nullptr));
   // Создание и аллокация буферов
-  m_A = vk_utils::createBuffer(m_device, sizeof(float) * m_length, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+  m_Source = vk_utils::createBuffer(m_device, sizeof(float) * m_length, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
                                                                        VK_BUFFER_USAGE_TRANSFER_DST_BIT);
-  m_B = vk_utils::createBuffer(m_device, sizeof(float) * m_length, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
-                                                                       VK_BUFFER_USAGE_TRANSFER_DST_BIT);
-  m_sum = vk_utils::createBuffer(m_device, sizeof(float) * m_length, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
-                                                                       VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
-  vk_utils::allocateAndBindWithPadding(m_device, m_physicalDevice, {m_A, m_B, m_sum}, 0);
+  m_Output = vk_utils::createBuffer(m_device, sizeof(float) * m_length, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+                                                                       VK_BUFFER_USAGE_TRANSFER_DST_BIT);                                                                      
+  vk_utils::allocateAndBindWithPadding(m_device, m_physicalDevice, {m_Source, m_Output}, 0);
 
   m_pBindings = std::make_shared<vk_utils::DescriptorMaker>(m_device, dtypes, 1);
 
   // Создание descriptor set для передачи буферов в шейдер
   m_pBindings->BindBegin(VK_SHADER_STAGE_COMPUTE_BIT);
-  m_pBindings->BindBuffer(0, m_A);
-  m_pBindings->BindBuffer(1, m_B);
-  m_pBindings->BindBuffer(2, m_sum);
+  m_pBindings->BindBuffer(0, m_Source);
+  m_pBindings->BindBuffer(1, m_Output);
   m_pBindings->BindEnd(&m_sumDS, &m_sumDSLayout);
 
-  // Заполнение буферов
   std::vector<float> values(m_length);
+  m_cpu.resize(m_length);
   for (uint32_t i = 0; i < values.size(); ++i) {
-    values[i] = (float)i;
+    values[i] = generateRandomFloat(-100.0, 100.0);
+    m_cpu[i] = values[i];
   }
-  m_pCopyHelper->UpdateBuffer(m_A, 0, values.data(), sizeof(float) * values.size());
+  m_pCopyHelper->UpdateBuffer(m_Source, 0, values.data(), sizeof(float) * values.size());
+
   for (uint32_t i = 0; i < values.size(); ++i) {
-    values[i] = (float)i * i;
+    values[i] = (float)0;
   }
-  m_pCopyHelper->UpdateBuffer(m_B, 0, values.data(), sizeof(float) * values.size());
+  m_pCopyHelper->UpdateBuffer(m_Output, 0, values.data(), sizeof(float) * values.size());
 }
 
 void SimpleCompute::BuildCommandBufferSimple(VkCommandBuffer a_cmdBuff, VkPipeline)
@@ -122,7 +132,7 @@ void SimpleCompute::BuildCommandBufferSimple(VkCommandBuffer a_cmdBuff, VkPipeli
 
   vkCmdPushConstants(a_cmdBuff, m_layout, VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(m_length), &m_length);
 
-  vkCmdDispatch(a_cmdBuff, 1, 1, 1);
+  vkCmdDispatch(a_cmdBuff, m_length / 1024 + 1, 1, 1);
 
   VK_CHECK_RESULT(vkEndCommandBuffer(a_cmdBuff));
 }
@@ -135,9 +145,8 @@ void SimpleCompute::CleanupPipeline()
     vkFreeCommandBuffers(m_device, m_commandPool, 1, &m_cmdBufferCompute);
   }
 
-  vkDestroyBuffer(m_device, m_A, nullptr);
-  vkDestroyBuffer(m_device, m_B, nullptr);
-  vkDestroyBuffer(m_device, m_sum, nullptr);
+  vkDestroyBuffer(m_device, m_Source, nullptr);
+  vkDestroyBuffer(m_device, m_Output, nullptr);
 
   vkDestroyPipelineLayout(m_device, m_layout, nullptr);
   vkDestroyPipeline(m_device, m_pipeline, nullptr);
@@ -202,11 +211,11 @@ void SimpleCompute::CreateComputePipeline()
 
 void SimpleCompute::Execute()
 {
+  srand(time(nullptr));
   SetupSimplePipeline();
   CreateComputePipeline();
 
   BuildCommandBufferSimple(m_cmdBufferCompute, nullptr);
-
   VkSubmitInfo submitInfo = {};
   submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
   submitInfo.commandBufferCount = 1;
@@ -217,15 +226,42 @@ void SimpleCompute::Execute()
   fenceCreateInfo.flags = 0;
   VK_CHECK_RESULT(vkCreateFence(m_device, &fenceCreateInfo, NULL, &m_fence));
 
+  std::cout << "InputBatch size: " << m_length << std::endl;
+
+  auto start = std::chrono::high_resolution_clock::now();
   // Отправляем буфер команд на выполнение
   VK_CHECK_RESULT(vkQueueSubmit(m_computeQueue, 1, &submitInfo, m_fence));
 
   //Ждём конца выполнения команд
   VK_CHECK_RESULT(vkWaitForFences(m_device, 1, &m_fence, VK_TRUE, 100000000000));
-
+  auto end = std::chrono::high_resolution_clock::now();
   std::vector<float> values(m_length);
-  m_pCopyHelper->ReadBuffer(m_sum, 0, values.data(), sizeof(float) * values.size());
-  for (auto v: values) {
-    std::cout << v << ' ';
+  m_pCopyHelper->ReadBuffer(m_Output, 0, values.data(), sizeof(float) * values.size());
+  float sum = 0.0;
+  for (int i = 0; i < m_length; ++i) {
+    sum += values[i];
   }
+  std::chrono::duration<float> duration = end - start;
+  // шейдер
+  std::cout << "Array sum: " << sum << std::endl;
+  std::cout << "Execution time shader (ms): " << duration.count() * 1000.f << std::endl;
+
+  std::vector<float> Output(m_length, 0);
+  sum = 0.0;
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < m_length; ++i) {
+    for (int j = -3; j <= 3; ++j) {
+      if (i + j < 0 || i + j >= m_length) {
+        continue;
+      }
+      Output[i] += (m_cpu[i + j] / 7.0f);
+    }
+    sum += Output[i];
+  }
+  end = std::chrono::high_resolution_clock::now();
+  duration = end - start;
+
+  // c++
+  std::cout << "Array sum: " << sum << std::endl;
+  std::cout << "Execution time CPU (ms): " << duration.count() * 1000. << std::endl;
 }
--- src/samples/simple_compute/simple_compute.h
+++ src/samples/simple_compute/simple_compute.h
@@ -79,7 +79,8 @@ private:
   VkPipeline m_pipeline;
   VkPipelineLayout m_layout;
 
-  VkBuffer m_A, m_B, m_sum;
+  VkBuffer m_Source, m_Output;
+  std::vector<float> m_cpu;
  
   void CreateInstance();
   void CreateDevice(uint32_t a_deviceId);
